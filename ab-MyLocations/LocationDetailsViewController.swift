a5667406f4bdef34729fceb18df4358b3b0067da
Aaron Bradley <abeats20@gmail.com>
2015-03-25 20:32:37 -0500

    did so many things, that I forgot them all...but it's going well

--- ab-MyLocations/LocationDetailsViewController.swift
+++ ab-MyLocations/LocationDetailsViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    return formatter
+}()
+
 class LocationDetailsViewController: UITableViewController {
 
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
+    var descriptionText = ""
+    var categoryName = "No Category"
 
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
@@ -25,8 +34,8 @@ class LocationDetailsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        descriptionTextView.text = ""
-        categoryLabel.text = ""
+        descriptionTextView.text = descriptionText
+        categoryLabel.text = categoryName
 
         latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
         longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
@@ -38,21 +47,132 @@ class LocationDetailsViewController: UITableViewController {
         }
 
         dateLabel.text = formatDate(NSDate())
+
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard:"))
+        gestureRecognizer.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(gestureRecognizer)
+
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        descriptionTextView.frame.size.width = view.frame.size.width - 30
+    }
+
+    func hideKeyboard(gestureRecognizer: UIGestureRecognizer) {
+        let point = gestureRecognizer.locationInView(tableView)
+        let indexPath = tableView.indexPathForRowAtPoint(point)
+
+        if indexPath != nil && indexPath!.section == 0 && indexPath!.row == 0 {
+            return
+        }
+
+        descriptionTextView.resignFirstResponder()
+    }
+
+
+    func formatDate(date: NSDate) -> String {
+        return dateFormatter.stringFromDate(date)
     }
 
     // MARK: - Other Methods
     func stringFromPlacemark(placemark: CLPlacemark) -> String {
         return
+            "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
+            "\(placemark.locality), " +
+            "\(placemark.administrativeArea) \(placemark.postalCode)," +
+            "\(placemark.country)"
 
     }
 
     // MARK: - IB Action Methods
 
     @IBAction func done() {
+        println("Description '\(descriptionText)'")
         dismissViewControllerAnimated(true, completion: nil)
     }
 
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
+
+    @IBAction func categoryPickerDidPickCategory(segue: UIStoryboardSegue) {
+        let controller = segue.sourceViewController as  CategoryPickerViewController
+        categoryName = controller.selectedCategoryName
+        categoryLabel.text = categoryName
+    }
+
+    // MARK: - UITableViewDelegate
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if indexPath.section == 0 || indexPath.section == 1 {
+            return indexPath
+        } else {
+            return nil
+        }
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            descriptionTextView.becomeFirstResponder()
+        }
+    }
+
+
+    // MARK: - Segue
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+        if segue.identifier == "PickCategory" {
+            let controller = segue.destinationViewController as CategoryPickerViewController
+            controller.selectedCategoryName = categoryName
+        }
+    }
+
 }
+
+
+// MARK: - EXTENSION
+extension LocationDetailsViewController: UITextViewDelegate {
+
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        descriptionText = (textView.text as NSString).stringByReplacingCharactersInRange(range, withString: text)
+        return true
+    }
+
+    func textViewDidEndEditing(textView: UITextView) {
+        descriptionText = textView.text
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
